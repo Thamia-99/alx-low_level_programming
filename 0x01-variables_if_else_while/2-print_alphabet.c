3a0b0809720c3cba777e9202fdbd5dfb4695fdc9
Nomathamsanqa <ngmsomi23@gmail.com>
2023-03-17 09:22:18 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+while (letter <= 'z')
+{
+    putchar(letter);  // print each letter
+    letter++;         // move to the next letter
+}
+
+putchar('\n');        // print a newline character
+
+return 0;
+
