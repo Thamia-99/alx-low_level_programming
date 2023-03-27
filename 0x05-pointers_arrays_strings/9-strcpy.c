42acc286481a446e11557a70281d5984f65bd732
Nomathamsanqa <ngmsomi23@gmail.com>
2023-03-27 05:02:35 -0700

    C pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * char *_strcpy - a function that copies a string pointed to by src 
+ * @dest: copy to 
+ * @src: copy from
+ * Retun: string 
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int 1 = 0;
+	int x = 0;
+
+	while (*(src + 1) != '\0')
+	{
+		l++;
+	}
+	for (; x < 1 ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[1] '\0';
+	return (dest);
+}
